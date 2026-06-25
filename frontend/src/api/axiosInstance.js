8e67e49bd8a24175cf754470acf2d505a2e7ed2d
sompalli jayadeep <sompallijayadeep@sompallis-MacBook-Air.local>
2026-06-25 22:18:22 +0530

    update Api Url from production

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spendwise-pur6.onrender.com//api",
+  baseURL: "https://spendwise-pur6.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
